155a4a74a4853855b488dee6f761ad351c2de576
f1uffee <antoine.lefloch@etik.com>
2024-01-16 11:28:50 +0100

    js selector created

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,18 @@
  *********************************************************************************/
 
 lancerJeu()
+
+let userInput = document.getElementById('inputEcriture')
+console.log(userInput)
+
+let userButton = document.getElementById('btnValiderMot')
+console.log(userButton)
+
+let proposalWord = document.querySelector('.zoneProposition')
+console.log(proposalWord)
+
+let displayScore = document.querySelector('.zoneScore')
+console.log(displayScore)
+
+let choicesButtons = document.querySelectorAll('.optionSource input')
+console.log(choicesButtons)
